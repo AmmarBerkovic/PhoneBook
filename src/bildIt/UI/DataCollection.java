e090db8e751e8891f970c5af8f5a3d2277c2327f
AmmarBerkovic <ammar.berkovic@gmail.com>
2019-04-02 16:51:15 +0200

    Dozvoljeno biranje bazepodataka

--- src/bildIt/UI/DataCollection.java
+++ src/bildIt/UI/DataCollection.java
@@ -2,7 +2,6 @@ package bildIt.UI;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 import bildIt.BO.Checks;
--- src/bildIt/main/App.java
+++ src/bildIt/main/App.java
@@ -13,7 +13,9 @@ public class App {
 	private static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) throws SQLException {
-		DLL.MajorWithdraw("users");
+		System.out.print("Pick database: ");
+		String str=input.next();
+		DLL.MajorWithdraw(str);
 		DLL.MinorWithdraw(Users.getUsers());
 		menu();
 	}
